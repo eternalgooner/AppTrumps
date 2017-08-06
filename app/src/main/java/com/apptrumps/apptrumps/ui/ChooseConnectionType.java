ca4f3ef86b36316a3e564e70f88a8b12dd8739d1
eternalgooner <eternalgooner@hotmail.com>
2017-08-06 20:50:28 +0100

    working on broadcast receiver for wifip2p

--- app/src/main/java/com/apptrumps/apptrumps/ui/ChooseConnectionType.java
+++ app/src/main/java/com/apptrumps/apptrumps/ui/ChooseConnectionType.java
@@ -3,6 +3,9 @@ package com.apptrumps.apptrumps.ui;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,28 +21,28 @@ public class ChooseConnectionType extends AppCompatActivity {
     private TextView txtMobileData;
     private TextView txtBluetooth;
     private static final String TAG = ChooseConnectionType.class.getSimpleName();
-    private WifiP2pManager wifiP2pManager;
-    private WifiP2pManager.Channel channel;
-    private WifiDirectReceiver receiver;
-    private IntentFilter intentFilter;
-    private WifiP2pManager.PeerListListener peerListListener;
+//    private WifiP2pManager wifiP2pManager;
+//    private WifiP2pManager.Channel channel;
+//    private WifiDirectReceiver receiver;
+//    private IntentFilter intentFilter;
+//    private WifiP2pManager.PeerListListener peerListListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_connection_type);
 
-        //register broadcst receiver
-        wifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = wifiP2pManager.initialize(this, getMainLooper(), null);
-        receiver = new WifiDirectReceiver(wifiP2pManager, channel, this);
-
-        //add intents that receiver checks for
-        intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+//        //register broadcst receiver
+//        wifiP2pManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+//        channel = wifiP2pManager.initialize(this, getMainLooper(), null);
+//        receiver = new WifiDirectReceiver(wifiP2pManager, channel, this);
+//
+//        //add intents that receiver checks for
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+//        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         getSupportActionBar().setTitle("Choose connection type");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -52,17 +55,17 @@ public class ChooseConnectionType extends AppCompatActivity {
         addClickListener(txtBluetooth);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(receiver, intentFilter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        registerReceiver(receiver, intentFilter);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        unregisterReceiver(receiver);
+//    }
 
     private void addClickListener(TextView view) {
         view.setOnClickListener(new View.OnClickListener() {
@@ -99,17 +102,43 @@ public class ChooseConnectionType extends AppCompatActivity {
 
     private void lookForWifiP2PConnections() {
         Log.d(TAG, "in lookForWifiP2PConnections()");
-        wifiP2pManager.discoverPeers(channel, new WifiP2pManager.ActionListener(){
-
-           @Override
-           public void onSuccess() {
-               Log.d(TAG, "in onSuccess() - device detected");
-           }
-
-           @Override
-           public void onFailure(int reason) {
-               Log.d(TAG, "in onFailure() - device not detected");
-           }
-        });
+        //TODO start new activity
+//        wifiP2pManager.discoverPeers(channel, new WifiP2pManager.ActionListener(){
+//
+//           @Override
+//           public void onSuccess() {
+//               Log.d(TAG, "in onSuccess() - device detected");
+//           }
+//
+//           @Override
+//           public void onFailure(int reason) {
+//               Log.d(TAG, "in onFailure() - device not detected");
+//           }
+//        });
     }
+
+//    @Override
+//    public void onPeersAvailable(WifiP2pDeviceList peers) {
+//        Log.d(TAG, "in onPeersAvailable() - num peers available: " + peers.getDeviceList().size());
+//
+//        final WifiP2pDevice device = peers.getDeviceList().iterator().next();
+//        WifiP2pConfig config = new WifiP2pConfig();
+//        config.deviceAddress = device.deviceAddress;
+//        wifiP2pManager.connect(channel, config, new WifiP2pManager.ActionListener() {
+//            @Override
+//            public void onSuccess() {
+//                Log.d(TAG, "connected successfully!!!! - - device name is: " + device.deviceName);
+//                Log.d(TAG, " - - device contents (int) are: " + device.describeContents());
+//                Log.d(TAG, " - - device address is: " + device.deviceAddress);
+//                Log.d(TAG, " - - device primary device type is: " + device.primaryDeviceType);
+//                Log.d(TAG, " - - device secondary device type is: " + device.secondaryDeviceType);
+//                Log.d(TAG, " - - device is group owner: " + device.isGroupOwner());
+//            }
+//
+//            @Override
+//            public void onFailure(int reason) {
+//                Log.d(TAG, "unsuccessful connection");
+//            }
+//        });
+//    }
 }
--- app/src/main/java/com/apptrumps/apptrumps/utils/WifiDirectReceiver.java
+++ app/src/main/java/com/apptrumps/apptrumps/utils/WifiDirectReceiver.java
@@ -3,10 +3,14 @@ package com.apptrumps.apptrumps.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
 import com.apptrumps.apptrumps.ui.ChooseConnectionType;
+import com.apptrumps.apptrumps.ui.ConnectAndPlay;
 
 /**
  * Created by David on 06-Aug-17.
@@ -15,14 +19,16 @@ import com.apptrumps.apptrumps.ui.ChooseConnectionType;
 public class WifiDirectReceiver extends BroadcastReceiver {
     private WifiP2pManager wifiP2pManager;
     private WifiP2pManager.Channel channel;
-    private ChooseConnectionType gameScreen;
+    private ConnectAndPlay gameScreen;
     private static final String TAG = WifiDirectReceiver.class.getSimpleName();
+    private WifiP2pManager.PeerListListener peerListListener;
 
-    public WifiDirectReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel, ChooseConnectionType gameScreen){
+    public WifiDirectReceiver(WifiP2pManager wifiP2pManager, WifiP2pManager.Channel channel, ConnectAndPlay gameScreen){
         super();
         this.wifiP2pManager = wifiP2pManager;
         this.channel = channel;
         this.gameScreen = gameScreen;
+        this.peerListListener = gameScreen;
     }
 
     @Override
@@ -32,7 +38,7 @@ public class WifiDirectReceiver extends BroadcastReceiver {
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             // Check to see if Wi-Fi is enabled and notify appropriate activity
-            Log.d(TAG, "wifi state changed");
+            Log.d(TAG, "1: wifi state changed");
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
                 // Wifi P2P is enabled
@@ -42,13 +48,17 @@ public class WifiDirectReceiver extends BroadcastReceiver {
                 Log.d(TAG, "wifi p2p is NOT enabled");
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-            Log.d(TAG, "list of peers has changed");
+            Log.d(TAG, "2: list of peers has changed");
+            if(wifiP2pManager != null){
+                Log.d(TAG, "wifi manager is not null so will request list of peers");
+                wifiP2pManager.requestPeers(channel, peerListListener);
+            }
             // Call WifiP2pManager.requestPeers() to get a list of current peers
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-            Log.d(TAG, "connection update - connect or disconnect");
+            Log.d(TAG, "3: connection update - connect or disconnect");
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-            Log.d(TAG, "this device wifi state has changed");
+            Log.d(TAG, "4: this device wifi state has changed");
             // Respond to this device's wifi state changing
         }
     }
